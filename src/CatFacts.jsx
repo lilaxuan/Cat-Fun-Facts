f82c067a0c8d4a12672806ea0fbe15e5626ff628
lilaxuan <106371956+lilaxuan@users.noreply.github.com>
2024-04-04 12:24:51 -0700

    Promises && Async-Await

--- src/CatFacts.jsx
+++ src/CatFacts.jsx
@@ -8,13 +8,27 @@ import { useState, useEffect } from 'react';
 function CatFacts() {
     const [catFacts, setCatFacts] = useState('');
 
-    function generateCatFact() {
+    // Using promise
+    // function generateCatFact() {
+    //     setCatFacts('Loading...');
+
+    //     axios.get('https://catfact.ninja/fact')
+    //         .then(response => {
+    //             setCatFacts(response.data.fact);
+    //         })
+    // }
+
+    // Using async && await
+    const generateCatFact = async () => {
         setCatFacts('Loading...');
 
-        axios.get('https://catfact.ninja/fact')
-            .then(response => {
-                setCatFacts(response.data.fact);
-            })
+        try {
+            const response = await axios.get('https://catfact.ninja/fact');
+            setCatFacts(response.data.fact);
+        } catch (error) {
+            setCatFacts('Failed to fetch cat fact. Please try again later.');
+            console.error('Error fetching cat fact:', error);
+        }
     }
 
     // When the page loads, call the generateCatFact once

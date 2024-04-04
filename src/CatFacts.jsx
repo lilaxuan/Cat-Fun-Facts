335ed5323b952cc301f11d6ab63299f795cc9dfa
lilaxuan <106371956+lilaxuan@users.noreply.github.com>
2024-04-04 12:08:49 -0700

    Make an API request && Display the data

--- src/CatFacts.jsx
+++ src/CatFacts.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import CatLogo from './assets/cat.svg'
 import './CatFacts.css';
+import { useState } from 'react';
 
-// insert your name here
+// Jiaxuan Li
 
 function CatFacts() {
+    const [catFacts, setCatFacts] = useState('');
 
     // you may need to add other code elsewhere!
 
@@ -12,27 +14,30 @@ function CatFacts() {
         axios.get('https://catfact.ninja/fact')
             .then(response => {
                 // insert code here
+                console.log(response.data.fact);
+                setCatFacts(response.data.fact);
             })
     }
 
 
 
-  return (
-    <div className="App">
-        <div className='catFactsText'>
-            {"Loading..." }
-            {/* The cat fact should be displayed here*/} 
+    return (
+        <div className="App">
+            <div className='catFactsText'>
+                {/* {"Loading..."} */}
+                {/* The cat fact should be displayed here*/}
+                {catFacts}
+            </div>
+            <div>
+                <button onClick={generateCatFact} className="catFactBtn">
+                    Click me for a cat fact!
+                </button>
+            </div>
+            <div>
+                <img src={CatLogo} className="catFactImg" />
+            </div>
         </div>
-        <div>
-            <button onClick={generateCatFact} className="catFactBtn">
-                Click me for a cat fact!
-            </button>
-        </div>
-        <div>
-            <img src={CatLogo} className="catFactImg" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default CatFacts
